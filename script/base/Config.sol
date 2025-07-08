3dff4349bd8aaf4dfaa312dbbc01523768753e4e
Joe Marron <marronjo@tcd.ie>
2025-07-08 17:31:04 +0100

    update sepolia config with market order hook and public tokens

--- script/base/Config.sol
+++ script/base/Config.sol
@@ -8,9 +8,9 @@ import {Currency} from "@uniswap/v4-core/src/types/Currency.sol";
 /// @notice Shared configuration between scripts
 contract Config {
     /// @dev populated with default anvil addresses
-    IERC20 constant token0 = IERC20(address(0x0165878A594ca255338adfa4d48449f69242Eb8F));
-    IERC20 constant token1 = IERC20(address(0xa513E6E4b8f2a923D98304ec87F64353C4D5C853));
-    IHooks constant hookContract = IHooks(address(0x0));
+    IERC20 constant token0 = IERC20(address(0x09fc36Bb906cB720037232697624bcAc48a4a21F));       // Cipher Token (CPH)
+    IERC20 constant token1 = IERC20(address(0x988E23405b307E59c0B63c71191FEB8681C15097));       // Mask Token (MSK)
+    IHooks constant hookContract = IHooks(address(0x34DEb2a90744fC6F2F133140dC69952Bb39CC080)); // Market Order Hook
 
     Currency constant currency0 = Currency.wrap(address(token0));
     Currency constant currency1 = Currency.wrap(address(token1));
